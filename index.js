a1e6f0ff21621540ff7b4ba0197e97c498dfd147
Youssef-Dahy <youssefdahy87@gmail.com>
2024-10-19 02:29:47 +0300

    edit index

--- index.js
+++ index.js
@@ -4,11 +4,77 @@ import { dbConnection } from "./database/connection.js";
 import { allRoutes } from "./src/modules/routes.js";
 import { AppError } from "./src/utils/AppError.js";
 import categoryRoutes from "./src/modules/category/category.routes.js";
-import { globalErrorHandling } from "./src/middleware/handleError.js";
+import {
+  globalErrorHandling,
+  handleError,
+} from "./src/middleware/handleError.js";
 import cors from "cors";
+import Stripe from "stripe";
+import cartModel from "./database/models/cart.model.js";
+import orderModel from "./database/models/order.model.js";
+import productModel from "./database/models/product.model.js";
+import userModel from "./database/models/user.model.js";
+const stripe = new Stripe(
+  "sk_test_51Q9Tpt2NUR7H5U3zlPuNCif8FA8DrDoRoFuhbSXk39O7Nyg4otRz3f8R2Iq6am5N5lRSVebUsxMod5fFohzlBNpN00dWCGXuz4"
+);
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.post(
+  "/order/webhook",
+  express.raw({ type: "application/json" }),
+  handleError(async (req, res, next) => {
+    const signature = req.headers["stripe-signature"].toString();
+    let event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      "whsec_0meXz6kcCe0ldeNvs8VkHpIlP4Xc9SoY"
+    );
+    let checkout;
+    let user = await userModel.findOne({ email: checkout.customer_email });
+
+    if (event.type == "checkout.session.completed") {
+      checkout = event.data.object;
+
+      let cart = await cartModel.findById({
+        _id: checkout.client_reference_id,
+      });
+
+      let totalOrderPrice = cart.totalPriceAfterDiscount
+        ? cart.totalPriceAfterDiscount
+        : cart.totalPrice;
+
+      let order = new orderModel({
+        user: user._id,
+        cartItems: cart.cartItems,
+        totalPrice: checkout.amount_total / 100,
+        shippingAddress: checkout.metadata,
+        paymentMethod: "online",
+        isPaid: true,
+      });
+
+      //bulk Write
+      if (order) {
+        let options = cart.cartItems.map((item) => ({
+          updateOne: {
+            filter: { _id: item.product },
+            update: { $inc: { quantity: -item.quantity, sold: item.quantity } },
+          },
+        }));
+
+        await productModel.bulkWrite(options);
+        await order.save();
+      } else {
+        return next(new AppError("Order not created", 400));
+      }
+
+      await cartModel.findByIdAndDelete(req.params.id);
+      res.json({ message: "Done", order });
+    }
+    res.status(200).json({ message: "success", checkout });
+  })
+);
 
 app.use(cors());
 app.use(express.json());
